26ad7c757ce283ea58b47f8aac6136a237452741
DeaconSteiner <deaconsteiner.it@gmail.com>
2025-09-02 18:21:08 -0600

    Completed Lab02

--- Lab02/Addition.cpp
+++ Lab02/Addition.cpp
@@ -2,8 +2,37 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-		
+int AddUp(int arr[], int n);
+
+int main(int argc, char *argv[]) {
+	int numbers = (argc - 1);
+
+	int A[10];
+
+	if (numbers > 10) {
+		cout << "No more than 10 integers, please." << endl;
+		return 1;
+	} else {
+		for (int i = 1; i < argc; i++)
+		{
+			A[i -1] = atoi(argv[i]);
+		}
+		cout << AddUp(A, numbers) << endl;
+	}
+	
+
+
+	
 	return 0;
+}
+
+int AddUp(int arr[], int n) {
+	int sum = 0;
+	
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];	
+	}
+	
+	return sum;
 }
--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Repo for all cosc-2030 labs and assignments
 ## Lab/Assignment completion log
 
 Lab 01 - Completed: August 26, 2025
+
+Lab02 - Completed: September 2, 2025
