9f86cd545487a842aeff03caa5109f56e0bcb69c
Deacon P. Steiner <dsteine2@hive1.eecs.uwyo.edu>
2025-09-08 17:49:33 -0600

    Began work on binary search program

---
+++ Lab03/binary_search/binary_search.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool binarySearch(vector<int> &v, int low, int high, int target);
+
+int main () {
+	vector<int> list = { 5, 24, 25, 47, 48, 59, 62, 63, 67, 92 };
+
+	int target = 48;
+
+	if (binarySearch(list, 0, list.size() - 1, target)) {
+		cout << target << " found." << endl;
+	} else {
+		cout << target << " not found." << endl;
+	}
+
+	return 0;
+}
+
+bool binarySearch(vector<int> &v, int low, int high, int target) {
+	// Range invalid - Base Case 1
+	if (low > high) { return false; }
+
+	// Finding mid-point
+	int mid = low + (high - low) / 2;
+
+	if (v[mid] == target) {
+		return true; // If middle element is target - Base Case 2
+	}
+
+	if (v[mid] > target) {
+		return binarySearch(v, low, mid - 1, target);
+	}
+
+	if (v[mid] < target) {
+		return binarySearch(v, mid + 1, high, target);
+	}
+}
+
