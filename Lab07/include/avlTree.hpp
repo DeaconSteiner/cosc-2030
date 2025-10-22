0a894284db7ea7dca44f3af2d2012d61f1b8dc65
DeaconSteiner <deaconsteiner.it@gmail.com>
2025-10-22 07:20:31 -0600

    Add project files. Add helper methods for rotations.

---
+++ Lab07/include/avlTree.hpp
@@ -0,0 +1,51 @@
+#ifndef AVL_TREE_HPP
+#define AVL_TREE_HPP
+
+struct Node {
+    int data;
+    int height;
+    Node *left, *right;
+};
+
+class avlTree {
+    private:
+        Node* root;
+        // private helper methods to keep API clean
+        bool search(int value, Node *node);
+        void insert(int value, Node *&node);
+        void remove(int value, Node *&node);
+        // constant methods do not modify the tree
+        void inorderTraversal(Node *node) const;
+        void preorderTraversal(Node *node) const;
+        void postorderTraversal(Node *node) const;
+        int minimum(Node *node) const;
+        int maximum(Node *node) const;
+        int inorderSuccessor(int value, Node *node) const;
+        int inorderPredecessor(int value, Node *node) const;
+       // this is a completely hidden function, only used for internal node tracking
+        Node* findNode(int value, Node* node) const;
+        // used for candidate tracking
+        Node* findpredecessorCandidate(int value, Node* node) const;
+        Node* findsuccessorCandidate(int value, Node* node) const;
+        Node* rotateLeft(Node* node);
+        Node* rotateRight(Node* node);
+       // cleanup, basically destructor logic
+        void deleteTree(Node *node);
+    public:
+        // Exposed methods, simplified for user interaction
+        avlTree();
+        ~avlTree();
+        bool search(int value);
+        void insert(int value);
+        void remove(int value);
+        // constant methods do not modify the tree
+        void inorderTraversal() const;
+        void preorderTraversal() const;
+        void postorderTraversal() const;
+        int minimum() const;
+        int maximum() const;
+        int inorderSuccessor(int value) const;
+        int inorderPredecessor(int value) const;
+};
+
+#endif
